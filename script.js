74d28d14c14dff25cbd115eabe78549f251238d5
Alej0xD <101449411+AlejoVampire@users.noreply.github.com>
2026-06-18 03:58:02 -0500

    Update service worker registration method

--- script.js
+++ script.js
@@ -368,6 +368,8 @@ function init() {
   renderView();
 }
 init();
-if (typeof navigator.serviceWorker !== 'undefined') {
-    navigator.serviceWorker.register('sw.js')
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('./sw.js');
+  });
 }
